edf731857345125b937e1af648f63904386b3417
Prashant <pk8873444412@gmail.com>
2024-11-29 00:47:53 +0530

    attr binding,style binding,class binding,unction binding,2 way data binding,attribute directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,40 @@
+import { NgClass, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgClass, NgStyle],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'code2learn-Angular';
+  myBtn: string = 'My Button';
+
+  // attr binding
+  isDisabled: boolean = true;
+
+  // style binding
+  bgColor: string = 'green';
+  titleColor: string = 'white';
+  description: string = 'font-size: 5@px; color: red';
+
+  // class binding
+  titleType = 'error';
+
+  //function binding
+  count = 0;
+  incrementCount() {
+    this.count++;
+  }
+
+  // 2 way data binding
+  myNum = 0;
+
+  // attribute directive
+  message = 'Hello World';
+  messageClass = 'greet';
+  messageTextTransform = 'uppercase';
 }
